40b2d03e8b0a6198bfde4c42b6a5ebf4d823fb8a
michael <michgboxy@gmail.com>
2017-07-24 00:02:32 -0700

    user model including the comment relationship

--- www/laravel/app/User.php
+++ www/laravel/app/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function tweet()
+    {
+
+        return $this->hasMany(tweet::class);
+    }
 }
