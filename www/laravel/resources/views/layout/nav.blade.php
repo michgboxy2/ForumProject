14ab1edac85bf1baa152f4328cda01375daa54cb
michael <michgboxy@gmail.com>
2017-07-24 04:22:29 -0700

    navigation bar

--- www/laravel/resources/views/layout/nav.blade.php
+++ www/laravel/resources/views/layout/nav.blade.php
@@ -5,7 +5,10 @@
       <li class="nav-item"><a href="#" class="nav-link"><span>Profile</span></a></li>
       <li class="nav-item"><a href="#" class="nav-link"><span>Mentions</span></a></li>
       <li class="nav-item"><a href="#" class="nav-link"><span>Logout</span></a></li> 
-      <li class="nav-item ml-auto"><a href="#" class="nav-link"><span>Michael</span></a></li>       
+      @if(Auth::check())
+      <li class="nav-item ml-auto"><a href="#" class="nav-link"><span>{{Auth::User()->name}}</span></a></li>  
+
+      @endif     
    </ul>
     </nav>
 
