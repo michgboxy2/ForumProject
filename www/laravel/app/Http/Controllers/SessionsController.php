21c3181890c5724e954063550ea96a62b7cb5653
michael <michgboxy@gmail.com>
2017-07-24 04:10:15 -0700

    handles user login

--- www/laravel/app/Http/Controllers/SessionsController.php
+++ www/laravel/app/Http/Controllers/SessionsController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
-    //
+
+	public function __construct(){
+
+		$this->middleware('guest', ['except' => 'destroy']);
+
+
+
+	}
+
+
+
+     public function create()
+
+     {
+     		return view('sessions.create');
+    	
+    }
+
+    public function store()
+    {
+    	if(!auth()->attempt(request(['email', 'password']))){
+    		return redirect('/login');
+    	}
+    	//enter dashboard
+    	return redirect('/');
+    }
+
+    public function destroy()
+
+    {
+
+    	auth()->logout();
+
+    	return redirect('/login');
+    }
 }
