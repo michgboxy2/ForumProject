626de5d89379c8beb1e048fa0681e9d6f3732198
michael <michgboxy@gmail.com>
2017-07-24 00:01:35 -0700

    includes the comment, tweet and relationship methods

--- www/laravel/app/tweet.php
+++ www/laravel/app/tweet.php
@@ -6,5 +6,29 @@ namespace App;
 
 class tweet extends Model
 {
-    
+    public function comments(){
+
+    	return $this->hasMany(comment::class);
+    }
+
+
+    public function addcomment($comment){
+
+    	$this->comments()->create(compact('comment'));
+
+
+
+    	// comment::create([
+
+     //    'comment' => request('comment'),
+
+     //    'tweet_id' => $this->id 
+
+     //    ]);
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
 }
